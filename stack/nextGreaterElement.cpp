6e1c49a721f51dee0bd6dd466db6f67f501aca70
Rishabh Kumar Gaud <65586827+Rishabh-Gaud@users.noreply.github.com>
2021-08-08 18:44:08 +0530

    Update nextGreaterElement.cpp

--- stack/nextGreaterElement.cpp
+++ stack/nextGreaterElement.cpp
@@ -1,4 +1,8 @@
- vector<long long> nextLargerElement(vector<long long> arr, int n){
+### Given an array arr[ ] of size N having distinct elements, the task is to find the next greater element for each element of the array in order of their appearance in the array.
+### Next greater element of an element in the array is the nearest element on the right which is greater than the current element.
+### If there does not exist next greater of current element, then next greater element for current element is -1. For example, next greater of the last element is always -1.
+
+vector<long long> nextLargerElement(vector<long long> arr, int n){
         // Your code here
         stack<long long >st;
         vector<long long>v;
