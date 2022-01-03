1d43eaf886c25e626e146439f1ddfd77cc6f3bda
Rishabh Kumar Gaud <65586827+Rishabh-Gaud@users.noreply.github.com>
2022-01-03 22:34:56 +0530

    Create Find_The_Town_Judge.cpp

---
+++ leetcode/january-challenge/Find_The_Town_Judge.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int>trusts(n+1),trusted(n+1);
+        for(auto &t:trust){
+            trusts[t[0]]++;
+            trusted[t[1]]++;
+        }
+        for(int i=1;i<=n;i++){
+            if(trusts[i]==0&&trusted[i]==n-1){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
