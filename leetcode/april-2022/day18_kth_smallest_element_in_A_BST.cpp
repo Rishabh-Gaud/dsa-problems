7e0337b9213d5df8663ef3f690004520d4f2d096
Rishabh Kumar Gaud <65586827+Rishabh-Gaud@users.noreply.github.com>
2022-04-18 08:38:15 +0530

    Create day18_kth_smallest_element_in_A_BST.cpp

---
+++ leetcode/april-2022/day18_kth_smallest_element_in_A_BST.cpp
@@ -0,0 +1,15 @@
+
+class Solution {
+public:
+    void inorder(TreeNode* root,vector<int>&v){
+        if(!root)return;
+        inorder(root->left,v);
+        v.push_back(root->val);
+        inorder(root->right,v);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        vector<int>v;
+        inorder(root,v);
+        return v[k-1];
+    }
+};
