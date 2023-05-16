9f39200085ea7021bf6830ccc6452713aa807dfa
Rishabh-Gaud <rishabh.kumbh@gmail.com>
2023-05-16 20:31:07 +0530

    Added solution - CodeSyncer

--- leetcode/144.-binary-tree-preorder-traversal/solution.cpp
+++ leetcode/144.-binary-tree-preorder-traversal/solution.cpp
@@ -21,7 +21,21 @@ public:
     }
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> ans;
-        printPreorderTraversal(root, ans);
+        stack<TreeNode*> st;
+        while(true) {
+            if(root != NULL) {
+                st.push(root);
+                ans.push_back(root -> val);
+                root = root -> left;
+            } else {
+                if(st.empty()) {
+                    break;
+                }
+                root = st.top();
+                st.pop();
+                root = root -> right;
+            }
+        }
         return ans;
     }
 };
