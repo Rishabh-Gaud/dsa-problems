791e76c6c5a892219d6ab411e9cead6e197a1ef8
Rishabh-Gaud <rishabh.kumbh@gmail.com>
2023-05-26 17:36:19 +0530

    Added solution - CodeSyncer

---
+++ leetcode/347.-top-k-frequent-elements/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int>mp;
+        for(auto it:nums){
+            mp[it]++;
+        }
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        for(auto &it:mp){
+            pq.push({it.second,it.first});
+            if(pq.size()>k)pq.pop();
+        }
+        vector<int>v;
+        while(pq.size()){
+            v.push_back(pq.top().second);pq.pop();
+        }
+        return v;
+    }
+};
