07fd4361b0f8508d02ca2d4e7d28fdb4f9772135
Rishabh-Gaud <rishabh.kumbh@gmail.com>
2023-05-27 12:17:03 +0530

    Added solution - CodeSyncer

--- leetcode/13.-roman-to-integer/solution.cpp
+++ leetcode/13.-roman-to-integer/solution.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
-    int romanToInt(string s) {
-       int answer = 0;
-       unordered_map<char, int>mp;
-       mp['I'] = 1;
-       mp['V'] = 5;
-       mp['X'] = 10;
-       mp['L'] = 50;
-       mp['C'] = 100;
-       mp['D'] = 500;
-       mp['M'] = 1000;
-       answer += mp[s[s.size() - 1]];
-       for(int i  = s.size() - 2; i >= 0; i--) {
-           if(mp[s[i]] >= mp[s[i + 1]]){
-               answer += mp[s[i]];
+    string intToRoman(int num) {
+        string ans;
+       vector<string>alpha = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" } ;
+       vector<int>number = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+       for(int i = 0; i < alpha.size(); i++) {
+        //    cout << alpha[i] << endl;;
+           while(num >= number[i]) {
+               ans += alpha[i];
+               num -= number[i];
            }
-           else answer -= mp[s[i]];
-       } 
-       return answer;
+       }
+       return ans;
     }
 };
