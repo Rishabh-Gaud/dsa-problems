352bb6267d998b625ea7b4d80f800b7ee1532d0f
Rishabh-Gaud <rishabh.kumbh@gmail.com>
2023-05-28 12:38:20 +0530

    Added solution - CodeSyncer

---
+++ leetcode/50.-powx-n/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 1.0;
+        long nn = abs(n);
+        while(nn > 0) {
+            if(nn % 2 == 0) {
+                x *= x;
+                nn /= 2;
+            }
+            else {
+                ans *= x;
+                nn--;
+            }
+        }
+        if(n < 0) {
+            ans = (double)(1.0) / (double) ans;
+        }
+        return ans;
+    }
+};
